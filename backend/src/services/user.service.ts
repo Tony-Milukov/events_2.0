1558e3f7388e560f0690a2de689d4c510439a06f
Tony-Milukov <89128409+Tony-Milukov@users.noreply.github.com>
2023-09-12 08:20:43 +0200

    bug fix

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -24,7 +24,7 @@ const loginService = async (email: string, password: string) => {
      if(user) {
          throw {errorMsg: "user with that email address is already registered",status:409}
      }
-     await bcrypt.hash(password,9, async (err:any,hash:any) => {
+      await bcrypt.hash(password,9, async (err:any,hash:any) => {
          //if there is an error, return an err message
          if(err) {
              throw {errorMsg: "something went wrong on hashing a password"}
@@ -38,8 +38,12 @@ const loginService = async (email: string, password: string) => {
          //if user is not, he is not created, throw error
          if (!user) {
              throw {errorMsg: "something went wrong on creating a user", status: 400}
-         } return jwt.sign({userId: user.id, username: user.username}, process.env.SECRET)
+         }
      })
+     //generate jwt
+     const newUser = User.findOne({where: {email}})
+     return jwt.sign({userId: newUser.id, username: newUser.username}, process.env.SECRET)
+
  }
 
 module.exports = {
