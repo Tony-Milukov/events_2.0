f0273657cb7bc7a9070c437ab9635859bb66af8f
Tony-Milukov <89128409+Tony-Milukov@users.noreply.github.com>
2023-09-12 07:59:00 +0200

    added status

--- backend/src/utilits/validators/request.validator.ts
+++ backend/src/utilits/validators/request.validator.ts
@@ -8,7 +8,7 @@ const bodyValidator = (req: any, res: any, name: string) => {
 const paramValidator = (req: any, res: any, name: string) => {
     const paramValue = req.params[name];
     if (!paramValue) {
-        throw {errorMsg: `data <<${name}>> was not sent in params request`}
+        throw {errorMsg: `data <<${name}>> was not sent in params request`, status: 400}
     }
     return paramValue;
 }
