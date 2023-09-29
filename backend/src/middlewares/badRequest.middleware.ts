b49d175d87a6dade9f2a385abd095f739a8efe18
Tony-Milukov <89128409+Tony-Milukov@users.noreply.github.com>
2023-09-29 13:51:56 +0200

    fixed bug

--- backend/src/middlewares/badRequest.middleware.ts
+++ backend/src/middlewares/badRequest.middleware.ts
@@ -1,7 +1,7 @@
-module.exports = (req:any,res:any) => {
-    res.json({
-        message: "Bad request",
-    }).status(400)
-}
-
-export {}
+// module.exports = (req:any,res:any) => {
+//     res.json({
+//         message: "Bad request",
+//     }).status(400)
+// }
+//
+// export {}
